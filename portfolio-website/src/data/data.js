423c9f3b51718df079ac0347854609da9c267019
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-28 23:07:37 -0800

    Fill out data file with real portfolio projects

--- portfolio-website/src/data/data.js
+++ portfolio-website/src/data/data.js
@@ -1,18 +1,78 @@
 export const softwareProjects = [
   {
     id: "1",
-    name: "cyberbug",
-    description: "an app for solving software bugs",
-    stack: "React, Rails",
+    name: "CyberBug",
+    description:
+      "CyberBug is a React application designed to help track and manage software bugs. As my final project with Flatiron School, this was an opportunity to combine everything taught over the program and create a true full stack application. Cyberbug utilizes a ReactJS frontend and Rails backend API, with MongoDb for persisting data. Inspired by the cyberpunk concept, I had a lot of fun with this project and wanted to end the program on a high note. I learned a ton about implementing a full React application and greatly expanded my CSS and UI design skills. ",
+    stack: "ReactJS, Rails API, Ruby, MongoDB",
     image: "https://picsum.photos/400",
+    githubLink: "https://github.com/kingsley113/Phase5-React-Cyberbug",
+    link: "TODO:",
   },
   {
     id: "2",
-    name: "who are you?",
-    description: "an app for finding your rpg background",
-    stack: "Javascript, Rails",
+    name: "CellMate V2 - ReactJS",
+    description:
+      "Revisiting my earlier version of Fallout Cascadia CellMate, after graduating from the Flatiron program, I set out to revamp and release CellMate to the Fallout Cascadia team. With this iteration I overhauled the front end to incorporate ReactJS as well as practice with Bootstrap UI and external libraries. After working with the team to include and refine features, the app is successfully deployed and currently in use.",
+    stack: "ReactJS, Bootstrap UI, Rails API, MongoDB,",
     image: "https://picsum.photos/400",
+    githubLink: "https://github.com/kingsley113/react-cell-mate",
+    link: "TODO:",
+  },
+  {
+    id: "3",
+    name: "Portfolio Website",
+    description:
+      "After completing the Flatiron Software Engineering Program, I needed a way to present and show off my projects. Rather than use a standard uninspired portfolio website template, I set out to create mine from scratch, to continue practicing my JavaScript and ReactJS skills, and once again pushing my understanding of CSS styling. I specifically chose design elements and effects that I didn’t already know how to create, as knowing how to learn new skills is essential. (and rewarding!)",
+    stack: "ReactJS, Javascript",
+    image: "https://picsum.photos/400",
+    githubLink: "https://github.com/kingsley113/Portfolio-Website",
+    link: "TODO:",
+  },
+  {
+    id: "4",
+    name: "Who Are You?",
+    description:
+      "‘Who Are You?’ was my first JavaScript application. The idea behind this app is to help generate a backstory for a role-playing game character. The user answers a series of questions or can select to roll a die and choose a random answer. After all questions are answered, a story is prepared and presented, inspired by the previously selected answers. The user can then save their story and also view other stories for further inspiration.",
+    stack: "Javascript, Rails API, Ruby, PostgreSQL",
+    image: "https://picsum.photos/400",
+    githubLink: "https://github.com/kingsley113/Phase4-JavascriptProject",
+    link: "TODO:",
+  },
+  {
+    id: "5",
+    name: "Fallout CellMate",
+    description:
+      "CellMate was created for the Fallout Cascadia development team to help track and manage the level design of the game world. The game engine divides the map is into a series of grid ‘cells’ and the team fell in love with the concept and design of the app. I set a goal to deploy this app for the team after completing the Flatiron program.",
+    stack: "HTML, CSS, Rails, Ruby, MongoDB",
+    image: "https://picsum.photos/400",
+    githubLink:
+      "https://github.com/kingsley113/Phase3-Rails-Project-CascadiaCellMate",
+    link: "TODO:",
+  },
+  {
+    id: "6",
+    name: "Krane Daily Reports",
+    description:
+      "Inspired by my construction background, this app is used to log and store daily construction reports. This was one of my first major steps into HTML & CSS and , in my typical fashion, pushed myself far beyond the requirements of the project. Throughout the design I kept the construction worker end user in mind and focused on making the UI very intuitive and quick to use. This project used vanilla HTML and CSS, while implementing Sinatra and Ruby for the backend routing.",
+    stack: "HTML, CSS, Sinatra, Ruby, MongoDB",
+    image: "https://picsum.photos/400",
+    githubLink: "https://github.com/kingsley113/Krane-Sinatra-Application",
+    link: "TODO:",
+  },
+  {
+    id: "7",
+    name: "SteamRoller",
+    description:
+      "Steamroller is a simple fun command line application to practice web scraping and creating Ruby Gems. This app will scan Steam’s top selling or newly released games page and extract the data, returning a list of game details which can then be sorted or used however the user wishes. While this can be used as a standalone project, the design intent is to incorporate this within other larger apps that could benefit from a dynamic source for Steam’s top games.",
+    stack: "Ruby, Nokogiri",
+    image: "https://picsum.photos/400",
+    githubLink: "https://github.com/kingsley113/steam_roller_cli_app",
+    link: "TODO:",
   },
 ];
+
 // TODO: Build this out with all of the info needed
 // TODO: Replace placeholders with real images
+// TODO: Add github links
+// TODO: Add project links
