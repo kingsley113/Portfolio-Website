c68253d037d777287f30c63256bc83f4a2c0da8d
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-30 01:26:36 -0800

    Add github link and icon to project cards

--- portfolio-website/src/components/main/projectCard.js
+++ portfolio-website/src/components/main/projectCard.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 
 class ProjectCard extends Component {
   render() {
-    const { name, description, stack, image, id } = this.props.project;
+    const { name, description, stack, image, id, githubLink } =
+      this.props.project;
     return (
       <div className="project-card">
         {/* {console.log(this.props.project)} */}
-        <img src={image} alt="screenshot" />
+        <img src={image} alt="screenshot" className="project-thumbnail" />
         {/* TODO: use image carousel? */}
         <div className="project-details">
           <h1>{name}</h1>
@@ -14,7 +15,13 @@ class ProjectCard extends Component {
           <p className="project-description">{description}</p>
         </div>
         <div className="flex-horizontal project-footer">
-          <div className="project-icon">TODO: git link</div>
+          {/* <div className="project-icon">TODO: git link</div> */}
+          <img
+            src="./icons/GitHub-Mark-Light-64px.png"
+            alt="Github Mark"
+            className="project-icon"
+            onClick={() => window.open(githubLink)}
+          />
           <div className="project-icon">TODO: link</div>
         </div>
       </div>
