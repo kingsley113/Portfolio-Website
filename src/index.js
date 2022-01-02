934abd2f3dc2775de76b5b79a5791df1be092d26
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-02 01:12:12 -0800

    Update React Router Home Path

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
     <Router basename={process.env.PUBLIC_URL}>
       {/* <Router> */}
       {/* <ParallaxProvider> */}
-      <Route exact path="/">
+      <Route path="/">
         <App />
       </Route>
       {/* </ParallaxProvider> */}
